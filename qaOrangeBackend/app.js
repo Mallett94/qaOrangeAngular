da5672cc52fa0a502172078183dcb1e9d3b0bfa3
Marc Edees <Marc@Marcs-MBP.home>
2017-08-25 16:55:46 +0100

    mongodb update

--- qaOrangeBackend/app.js
+++ qaOrangeBackend/app.js
@@ -4,6 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/cinemadb');
+const mongoClient = require('mongodb');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,14 +19,58 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//Mongo
+//Films
+app.get('/FilmsAll', (req, res) => {
+  mongoClient.connect('mongodb://localhost/cinemadb', function(err, db){
+    const FilmsAll = db.collection('films').find({film_name:'Dunkirk'}, {'__v':0, '_id':0}).toArray(function(err, docs){
+      filmsArray = docs;
+    })
+  });
+  if (typeof filmsArray !== 'undefined'){
+    res.json(filmsArray);
+  } else {
+    res.json([]);
+  }
+});
+//Locations
+app.get('/Locations', (req, res) => {
+  mongoClient.connect('mongodb://localhost/cinemadb', function(err, db){
+    const Locations = db.collection('locations').find({},{'_id':0, '__v':0}).toArray(function(err, docs){
+      locationsArray = docs;
+    })
+  });
+  if (typeof locationsArray !== 'undefined'){
+    res.json(locationsArray);
+  } else {
+    res.json([]);
+  }
+});
+//Prices
+app.get('/Prices', (req, res) => {
+  mongoClient.connect('mongodb://localhost/cinemadb', function(err, db){
+    const Prices = db.collection('prices').find({},{'_id':0, '__v':0}).toArray(function(err, docs){
+      pricesArray = docs;
+    })
+  });
+  if (typeof pricesArray !== 'undefined'){
+    res.json(pricesArray);
+  } else {
+    res.json([]);
+  }
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/showings");
+  res.header("Access-Control_ALlow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/', index);
 app.use('/users', users);
 
